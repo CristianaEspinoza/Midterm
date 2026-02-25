5c700553eb464dfe97f8e8a6b88ad51877e11cee
Cristiana Espinoza <cristianaespinoza256@gmail.com>
2026-02-25 09:09:07 -0600

    mongo

--- mongo/server-mongo.js
+++ mongo/server-mongo.js
@@ -95,5 +95,40 @@ app.get('/books', async (req,res)=>{
     }
 
 })
+//Get book by id
+app.get('/books/:id', checkIfBookExists, async (req,res)=>{
+    res.json(req.dbBook)
+})  
+//Create a new book
+app.post('/books', payloadCheck, validateBookFields, async (req,res)=>{
+    const {title, author, category, price} = req.body;
+    try {
+        const book = await Book.create({title, author, category, price});
+        res.status(201).json(book);     
+    } catch (error) {
+        res.status(500).json({message: "Db error"})
+    }   
+})
+//Update a book 
+app.put('/books/:id', checkIfBookExists, payloadCheck, validateBookFields, async (req,res)=>{
+    const {title, author, category, price} = req.body;          
+    try {
+        await req.dbBook.update({title, author, category, price});
+        res.json(req.dbBook);   
+    } catch (error) {
+        res.status(500).json({message: "Db error"})
+    }
+})
+//Delete a book'
+app.delete('/books/:id', checkIfBookExists, async (req,res)=>{
+    try {
+        await req.dbBook.delete();
+        res.json({message: "Book deleted"})     
+    } catch (error) {
+        res.status(500).json({message: "Db error"})
+    }
+})
+
+
 
 app.listen(9000, () => console.log("Server is running on port 9000"))
